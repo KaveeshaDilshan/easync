eb4e302f50a1755ebb0eb8dd2e9b1ae436fed483
KaveeshaDilshan <kaveesha.18@cse.mrt.ac.lk>
2023-01-03 00:59:09 +0530

    test 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,9 @@ import 'package:path_provider/path_provider.dart';
 
 import 'detailfolder.dart';
 
-
 void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
